5bcf54fe84a71a7a74619e0eb1339aa332b4a988
k98940 <pv1xxb@yandex.ru>
2023-12-25 21:04:18 +0600

    init: isPositive, getMaxNumber, canQueenCaptureKing. (Mon, Dec 25, 2023 09:04:18 PM)

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,8 +21,8 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  return number >= 0;
 }
 
 /**
@@ -38,8 +38,9 @@ function isPositive(/* number */) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  const ructor = a > b ? a : b;
+  return ructor > c ? ructor : c;
 }
 
 /**
@@ -60,8 +61,11 @@ function getMaxNumber(/* a, b, c */) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === king.x || queen.y === king.y) return true;
+  const dX = Math.abs(queen.x - king.x);
+  const dY = Math.abs(queen.y - king.y);
+  return dX === dY;
 }
 
 /**
